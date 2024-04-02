ebebda42807095c46708dd415fbfb123c985a409
Ivan Imbert <ivan.imbert@epita.fr>
2024-04-02 16:35:44 +0200

    feat: Solving 3 unknown systems of equations using matrices and determinants

--- src/main/java/org.goblivend.rayxploring/Utils/MathUtils.java
+++ src/main/java/org.goblivend.rayxploring/Utils/MathUtils.java
@@ -1,5 +1,7 @@
 package org.goblivend.rayxploring.Utils;
 
+import org.goblivend.rayxploring.components.Ray;
+
 import java.util.Random;
 
 import static java.lang.Double.*;
@@ -54,11 +56,11 @@ public class MathUtils {
     }
 
     public static double disperseAngle(Random rd, double angle, double radDisp) {
-        return angle + rd.nextDouble()*radDisp - (radDisp / 2);
+        return angle + rd.nextDouble() * radDisp - (radDisp / 2);
     }
 
     public static Vector2D disperseAngles(Random rd, double radDisp) {
-        return new Vector2D(rd.nextDouble() * 2*PI, rd.nextDouble() * radDisp);
+        return new Vector2D(rd.nextDouble() * 2 * PI, rd.nextDouble() * radDisp);
     }
 
     public static Vector2D disperseVector(Random rd, Vector2D dir, double degreesDisp) {
@@ -73,7 +75,7 @@ public class MathUtils {
     }
 
     public static double scalarProduct(Vector3D v1, Vector3D v2) {
-        return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() *v2.z();
+        return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
     }
 
     public static boolean doubleEqual(double d1, double d2) {
@@ -86,16 +88,18 @@ public class MathUtils {
         double alpha1 = v1S.x();
         double beta1 = v1S.y();
 
-        double alpha2 = doubleEqual(sin(beta1), 0) ? 0 : alpha1 + PI/2; // +- PI/2
-        double beta2 = PI/2;
+        double alpha2 = doubleEqual(sin(beta1), 0) ? 0 : alpha1 + PI / 2; // +- PI/2
+        double beta2 = PI / 2;
 
-        double alpha3 = alpha2 + PI/2; // +- PI/2
+        double alpha3 = alpha2 + PI / 2; // +- PI/2
         double beta3 = atan2(-cos(beta1), sin(beta1) * cos(alpha1 - alpha3));
 
         return new Tuple<>(anglesToVector3d(new Vector2D(alpha2, beta2)), anglesToVector3d(new Vector2D(alpha3, beta3)));
     }
 
-    /** Returns the roots of any polynomial of the shape aX^2 + bX + c
+    /**
+     * Returns the roots of any polynomial of the shape aX^2 + bX + c
+     *
      * @param a
      * @param b
      * @param c constant
@@ -103,17 +107,17 @@ public class MathUtils {
      */
     public static Tuple<Double, Double> roots(double a, double b, double c) {
         // delta = b - 4*a*c
-        double delta = Math.pow(b, 2) - 4*a*c;
+        double delta = Math.pow(b, 2) - 4 * a * c;
 
         // delta < 0 ? return null
         if (delta < 0)
             return null;
 
         // t1 = (-b - sqrt(delta)) / (2*a)
-        double t1 = (-b - Math.sqrt(delta)) / (2*a);
+        double t1 = (-b - Math.sqrt(delta)) / (2 * a);
 
         // t2 = (-b + sqrt(delta)) / (2*a)
-        double t2 = (-b + Math.sqrt(delta)) / (2*a);
+        double t2 = (-b + Math.sqrt(delta)) / (2 * a);
 
         return new Tuple<>(t1, t2);
     }
@@ -122,6 +126,7 @@ public class MathUtils {
     /**
      * Gets the smallest positive root out of a 2nd degree polynomial
      * of the shape aX^2 + bX + c = 0
+     *
      * @param a
      * @param b
      * @param c constant
@@ -142,5 +147,102 @@ public class MathUtils {
 
         // t2 < t1
         return roots.t2();
+    }
+
+    /**
+     * Function calculating the time and coordinate of a ray hitting a plane from a 3d model
+     *
+     * @param plane  X and Y vectors describing the arrival plane ()
+     * @param origin The point of origin of the plane
+     * @param ray    The ray
+     * @return a 3-tuple containing in first position the time at which the ray reaches the plane, then the
+     */
+    public static Tuple3<Double, Double, Double> intersection(Tuple<Vector3D, Vector3D> plane, Vector3D origin, Ray<Vector3D> ray) {
+        double X = ray.pos().x() - origin.x();
+        double Y = ray.pos().y() - origin.y();
+        double Z = ray.pos().z() - origin.z();
+
+        double dX = ray.dir().x();
+        double dY = ray.dir().y();
+        double dZ = ray.dir().z();
+
+        double X1 = plane.t1().x();
+        double Y1 = plane.t1().y();
+        double Z1 = plane.t1().z();
+
+        double X2 = plane.t2().x();
+        double Y2 = plane.t2().y();
+        double Z2 = plane.t2().z();
+
+        double delta = determinant(new Double[][] {
+                {X1, X2, -dX},
+                {Y1, Y2, -dY},
+                {Z1, Z2, -dZ},
+        });
+
+        double t = determinant(new Double[][]{
+                {X1, X2, X},
+                {Y1, Y2, Y},
+                {Z1, Z2, Z}
+        }) / delta;
+
+        double lambda1 = determinant(new Double[][]{
+                {X, X2, -dX},
+                {Y, Y2, -dY},
+                {Z, Z2, -dZ}
+        }) / delta;
+
+        double lambda2 = determinant(new Double[][]{
+                {X1, X, -dX},
+                {Y1, Y, -dY},
+                {Z1, Z, -dZ}
+        }) / delta;
+
+
+
+        /*
+        // Only works if dX != 0 so in such a case rotate the vectors
+        if (dX == 0) {
+            return intersection(
+                    new Tuple<>(
+                            new Vector3D(Y1, Z1, X1),
+                            new Vector3D(Y2, Z2, X2)),
+                    new Vector3D(origin.y(), origin.z(), origin.x()),
+                    new Ray<>(
+                            new Vector3D(ray.pos().y(), ray.pos().z(), ray.pos().x()),
+                            new Vector3D(dY, dZ, dX),
+                            ray.color()
+                    )
+            );
+        }
+
+
+        double A = X2 / dX + (X1 * (X2 * dY - Y2 * dX)) / (dX * (Y1 * dX - X1 * dY));
+        double B = -X1 / dX * (X * dY + Y * dX) / (Y1 * dX - X1 * dY) - X / dX;
+        double C = (X2 * dY - Y2 * dX) / (Y1 * dX - X1 * dY);
+        double D = (X * dY + Y * dX) / (Y1 * dX - X1 * dY);
+
+        double lambda2 = (B * dZ - Z + D * Z1) / (Z2 - A * dZ - C * Z1);
+        double lambda1 = (lambda2 * (X2 * dY - Y2 * dX) - X * dY + Y * dX) / (Y1 * dX - X1 * dY);
+        double t = (lambda1 * X1 + lambda2 * X2 - X) / dX;*/
+
+        return new Tuple3<>(t, lambda1, lambda2);
+    }
+
+    public static double determinant(Double[][] m) {
+        if (m.length == 0 || m.length != m[0].length) {
+            throw new IllegalArgumentException();
+        }
+
+        if (m.length == 2) {
+            return m[0][0]*m[1][1] - m[0][1]*m[1][0];
+        }
+
+        if (m.length == 3) {
+            return m[0][0] * determinant(new Double[][] {{m[1][1], m[1][2]}, {m[2][1], m[2][2]}})
+                    - m[1][0] * determinant(new Double[][] {{m[0][1], m[0][2]}, {m[2][1], m[2][2]}})
+                    + m[2][0] * determinant(new Double[][] {{m[0][1], m[0][2]}, {m[1][1], m[1][2]}});
+        }
+        throw new IllegalArgumentException("Determinant not defined for n not in [2, 3]");
     }
 }
--- src/test/java/org/goblivend/rayxploring/Utils/MathUtilsTest.java
+++ src/test/java/org/goblivend/rayxploring/Utils/MathUtilsTest.java
@@ -1,12 +1,15 @@
 package org.goblivend.rayxploring.Utils;
 
+import org.goblivend.rayxploring.components.Ray;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.awt.*;
 import java.util.stream.Stream;
 
 import static java.lang.Double.NaN;
+import static java.lang.Double.isNaN;
 import static java.lang.Math.PI;
 import static java.lang.String.format;
 import static org.goblivend.rayxploring.Utils.MathUtils.*;
@@ -16,10 +19,10 @@ class MathUtilsTest {
     private static Stream<Arguments> angle2DTestsProvider() {
         return Stream.of(
                 Arguments.of(0d, new Vector2D(1, 0)),
-                Arguments.of(PI/2, new Vector2D(0, 1)),
+                Arguments.of(PI / 2, new Vector2D(0, 1)),
                 Arguments.of(PI, new Vector2D(-1, 0)),
-                Arguments.of(-PI/2, new Vector2D(0, -1)),
-                Arguments.of(PI/4, new Vector2D(0.7, 0.7)),
+                Arguments.of(-PI / 2, new Vector2D(0, -1)),
+                Arguments.of(PI / 4, new Vector2D(0.7, 0.7)),
                 Arguments.of(NaN, new Vector2D(0, 0))
         );
     }
@@ -30,21 +33,21 @@ class MathUtilsTest {
         double actual = vector2dToAngle(point);
         assertEquals(expected, actual, 0.01d);
     }
+
     @ParameterizedTest
     @MethodSource("angle2DTestsProvider")
     void angleToPoint2DTests(Double angle, Vector2D expected) {
         Vector2D actual = angleToVector2d(angle);
-        assertEquals(expected.x(), actual.x(), 0.01d);
-        assertEquals(expected.y(), actual.y(), 0.01d);
+        assertVectors(expected, actual);
     }
 
     private static Stream<Arguments> angle3DTestsProvider() {
         return Stream.of(
-                Arguments.of(new Vector2D(0, PI/2), new Vector3D(1, 0, 0)),
+                Arguments.of(new Vector2D(0, PI / 2), new Vector3D(1, 0, 0)),
                 Arguments.of(new Vector2D(0, 0), new Vector3D(0, 0, 1)),
-                Arguments.of(new Vector2D(PI/2, PI/2), new Vector3D(0, 1, 0)),
-                Arguments.of(new Vector2D(PI, PI/2), new Vector3D(-1, 0, 0)),
-                Arguments.of(new Vector2D(PI/4, PI/2), new Vector3D(0.7, 0.7, 0)),
+                Arguments.of(new Vector2D(PI / 2, PI / 2), new Vector3D(0, 1, 0)),
+                Arguments.of(new Vector2D(PI, PI / 2), new Vector3D(-1, 0, 0)),
+                Arguments.of(new Vector2D(PI / 4, PI / 2), new Vector3D(0.7, 0.7, 0)),
                 Arguments.of(new Vector2D(0, PI), new Vector3D(0, 0, -1))
         );
     }
@@ -53,16 +56,15 @@ class MathUtilsTest {
     @MethodSource("angle3DTestsProvider")
     void point3DToAngleTests(Vector2D expected, Vector3D point) {
         Vector2D actual = vector3dToAngles(point);
-        assertEquals(expected.x(), actual.x(), 0.01d);
-        assertEquals(expected.y(), actual.y(), 0.01d);
+        assertVectors(expected, actual);
     }
+
     @ParameterizedTest
     @MethodSource("angle3DTestsProvider")
     void angleToPoint3DTests(Vector2D angle, Vector3D expected) {
         Vector3D actual = anglesToVector3d(angle);
-        assertEquals(expected.x(), actual.x(), 0.01d);
-        assertEquals(expected.y(), actual.y(), 0.01d);
-        assertEquals(expected.z(), actual.z(), 0.01d);
+
+        assertVectors(expected, actual);
     }
 
     private static Stream<Arguments> scalarProduct3DTestsProvider() {
@@ -109,5 +111,79 @@ class MathUtilsTest {
         assertEquals(0, scalarProduct(base.t1(), base.t2()), 0.01d, format("\nfrom v: %s,\nv1: %s,\nv2:%s\n", v, base.t1(), base.t2()));
         assertEquals(0, scalarProduct(v, base.t2()), 0.01d);
         assertEquals(0, scalarProduct(base.t1(), v), 0.01d);
+    }
+
+    private static Stream<Arguments> planeIntersectionValidTestsProvider() {
+        return Stream.of(
+                Arguments.of(new Tuple<>(new Vector3D(1, 0, 0), new Vector3D(0, 1, 0)), new Vector3D(0, 0, 5), new Ray<>(new Vector3D(0, 0, 0), new Vector3D(0, 0, 1), Color.BLACK)),
+                Arguments.of(new Tuple<>(new Vector3D(0, 1, 0), new Vector3D(1, 0, 0)), new Vector3D(0, 0, 5), new Ray<>(new Vector3D(0, 0, 0), new Vector3D(0, 0, 1), Color.BLACK)),
+                Arguments.of(new Tuple<>(new Vector3D(0, 1, 0), new Vector3D(0, 0, 1)), new Vector3D(5, 0, 0), new Ray<>(new Vector3D(0, 0, 0), new Vector3D(1, 0, 0), Color.BLACK)),
+                Arguments.of(new Tuple<>(new Vector3D(0, 0, 1), new Vector3D(1, 0, 0)), new Vector3D(0, 5, 0), new Ray<>(new Vector3D(0, 0, 0), new Vector3D(0, 1, 0), Color.BLACK)),
+                Arguments.of(new Tuple<>(new Vector3D(1, 0, 0), new Vector3D(0, 1, 0)), new Vector3D(0, 0, 5), new Ray<>(new Vector3D(0, 0, 0), new Vector3D(0, 0, -1), Color.BLACK)),
+                Arguments.of(new Tuple<>(new Vector3D(1, 0, 0), new Vector3D(0, 1, 0)), new Vector3D(0, 0, 5), new Ray<>(new Vector3D(0, 0, 0), new Vector3D(0, 0, 1), Color.BLACK))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("planeIntersectionValidTestsProvider")
+    void intersectPlaneValidTest(Tuple<Vector3D, Vector3D> plane, Vector3D origin, Ray<Vector3D> ray) {
+        Tuple3<Double, Double, Double> actual = intersection(plane, origin, ray);
+
+        double t = actual.t1();
+        double lambda1 = actual.t2();
+        double lambda2 = actual.t3();
+
+        assertFalse(isNaN(t));
+        assertFalse(isNaN(lambda1));
+        assertFalse(isNaN(lambda2));
+
+        Vector3D rayArrived = ray.pos().translate(ray.dir(), t);
+        Vector3D planePosition = origin.translate(plane.t1(), lambda1).translate(plane.t2(), lambda2);
+
+        assertVectors(rayArrived, planePosition);
+    }
+
+    private static Stream<Arguments> determinantTestsProvider() {
+        return Stream.of(
+                Arguments.of(new Double[][] {
+                        {1d, 0d}, {0d, 1d}
+                }, 1d),
+                Arguments.of(new Double[][] {
+                        {0d, 1d}, {1d, 0d}
+                }, -1d),
+                Arguments.of(new Double[][] {
+                        {1d, 1d}, {1d, 1d}
+                }, 0d),
+                Arguments.of(new Double[][] {
+                        {1d, 2d}, {3d, 4d}
+                }, -2d),
+                Arguments.of(new Double[][] {
+                        {1d, 0d, 0d}, {0d, 1d, 0d}, {0d, 0d, 1d}
+                }, 1d),
+                Arguments.of(new Double[][] {
+                        {1d, 5d, 3d}, {2d, 4d, 7d}, {4d, 6d, 2d}
+                }, 74d)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("determinantTestsProvider")
+    void determinantValidTest(Double[][] m, Double d) {
+        assertEquals(d, determinant(m), 0.01d);
+    }
+
+
+
+
+
+    private void assertVectors(Vector2D v1, Vector2D v2) {
+        assertEquals(v1.x(), v2.x(), 0.01d);
+        assertEquals(v1.y(), v2.y(), 0.01d);
+    }
+
+    private void assertVectors(Vector3D v1, Vector3D v2) {
+        assertEquals(v1.x(), v2.x(), 0.01d);
+        assertEquals(v1.y(), v2.y(), 0.01d);
+        assertEquals(v1.z(), v2.z(), 0.01d);
     }
 }
